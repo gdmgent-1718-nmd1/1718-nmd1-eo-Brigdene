b21934fa391ec1fcb62baf9e54f24482ca51a5cb
Brighton De Neef <brigdene@student.arteveldehs.be>
2018-03-26 17:29:39 +0200

    Made A JavaScript for the togglers

---
+++ pages/js.js
@@ -0,0 +1,4 @@
+let buttonColors = document.getElementById('colorToggler');
+let buttonText = document.getElementById('textToggler');
+let buttonButtons = document.getElementById('buttonToggler');
+let buttonImages = document.getElementById('imageToggler');
